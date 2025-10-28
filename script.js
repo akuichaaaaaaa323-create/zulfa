b376b78028835d0055def9d72278c8364b1e65cd
akuichaaaaaaa323-create <akuichaaaaaaa323@gmail.com>
2025-10-28 11:27:22 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+conts tombolSapa = document.getElementByld('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click',function() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+                            aler ('Halo! Terima kasih sudah berkunjung!');
+});
